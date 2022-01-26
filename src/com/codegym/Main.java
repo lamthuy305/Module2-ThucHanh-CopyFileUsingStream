72b3f28ac07769eeee317a38d10a8ee28dc1a852
lamthuy305 <lamthuy305@gmail.com>
2022-01-26 21:01:51 +0700

    done

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,43 @@
+package com.codegym;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    private static void copyFileUsingStream(File source, File dest) throws IOException {
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = new FileInputStream(source);
+            os = new FileOutputStream(dest);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+            }
+        } finally {
+            assert is != null;
+            is.close();
+            assert os != null;
+            os.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter source file:");
+        String sourcePath = sc.nextLine();
+        System.out.println("Enter destination file:");
+        String destPath = sc.nextLine();
+
+        File sourceFile = new File(sourcePath);
+        File destFile = new File(destPath);
+        try {
+            copyFileUsingStream(sourceFile, destFile);
+            System.out.println("Copy completed");
+        } catch (IOException ioe) {
+            System.out.println("Can't copy that file");
+            System.out.println(ioe.getMessage());
+        }
+    }
+}
